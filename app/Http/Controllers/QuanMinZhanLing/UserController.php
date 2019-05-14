50bff8f59c0e6b969d3036cd9b474ecd9fab4a60
wanghan <minglongoc@sina.com>
2019-05-14 10:35:11 +0800

    '123'

--- app/Http/Controllers/QuanMinZhanLing/UserController.php
+++ app/Http/Controllers/QuanMinZhanLing/UserController.php
@@ -388,9 +388,7 @@ class UserController extends BaseController
     public function shareOnePicture()
     {
         // create Image from file
-        $img = Image::make('public/test.svg');
-
-        dd($img);
+        $img = Image::make('public/test.jpg');
 
         // write text
         $img->text('The quick brown fox jumps over the lazy dog.');
@@ -399,14 +397,14 @@ class UserController extends BaseController
         $img->text('The quick brown fox jumps over the lazy dog.', 120, 100);
 
         // use callback to define details
-        $img->text('foo', 0, 0, function($font) {
-            $font->file('foo/bar.ttf');
-            $font->size(24);
-            $font->color('#fdf6e3');
-            $font->align('center');
-            $font->valign('top');
-            $font->angle(45);
-        });
+//        $img->text('foo', 0, 0, function($font) {
+//            $font->file('foo/bar.ttf');
+//            $font->size(24);
+//            $font->color('#fdf6e3');
+//            $font->align('center');
+//            $font->valign('top');
+//            $font->angle(45);
+//        });
 
         // draw transparent text
         $img->text('foo', 0, 0, function($font) {
