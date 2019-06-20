37bd8eb9af037f3fdb1300da5d389bb8de38da78
wanghan <minglongoc@sina.com>
2019-06-20 14:14:06 +0800

    '321'

--- app/Http/Controllers/QuanMinZhanLing/Temp/MyTempController.php
+++ app/Http/Controllers/QuanMinZhanLing/Temp/MyTempController.php
@@ -9,19 +9,7 @@ class MyTempController extends BaseController
 {
     public function test()
     {
-        $Geo=new \Geohash\GeoHash();
 
-        $lng=\sprintf("%.4f",'108.6548');
-        $lat=\sprintf("%.4f",'40.4503');
-
-        $geohash=$Geo->encode($lat,$lng,'8');
-
-
-        $res=Redis::connection('tssjOldToNew')->set('wanghan','duan');
-        $res=Redis::connection('tssjOldToNew')->get('wanghan');
-
-
-        dd($res);
     }
 
 
