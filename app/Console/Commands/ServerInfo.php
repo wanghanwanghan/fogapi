79a9f9cfea8573ddba7c540a1eca680cd3e1a5d1
wanghan <minglongoc@sina.com>
2019-07-18 10:04:54 +0800

    '123'

--- app/Console/Commands/ServerInfo.php
+++ app/Console/Commands/ServerInfo.php
@@ -30,118 +30,113 @@ class ServerInfo extends Command
 
     public function handle()
     {
-        for ($i=1;$i<=3;$i++)
+        $fp=popen("top -b -n 2 | egrep 'Cpu|KiB Mem|KiB Swap'","r");
+
+        $rs="";
+
+        while(!feof($fp))
         {
-            $fp=popen("top -b -n 2 | egrep 'Cpu|KiB Mem|KiB Swap'","r");
+            $rs .= fread($fp,1024);
+        }
 
-            $rs="";
+        pclose($fp);
 
-            while(!feof($fp))
+        $res=array_filter(explode(PHP_EOL,$rs));
+
+        $info=[];
+        foreach ($res as $one)
+        {
+            $tmp=explode(' ',$one);
+
+            preg_match_all('/Cpu/',$tmp[0],$thisRes);
+
+            if (!empty(current($thisRes)))
             {
-                $rs .= fread($fp,1024);
-            }
-
-            pclose($fp);
-
-            $res=array_filter(explode(PHP_EOL,$rs));
-
-            $info=[];
-            foreach ($res as $one)
-            {
-                $tmp=explode(' ',$one);
-
-                preg_match_all('/Cpu/',$tmp[0],$thisRes);
-
-                if (!empty(current($thisRes)))
+                foreach ($tmp as $cpu)
                 {
-                    foreach ($tmp as $cpu)
+                    if (is_numeric($cpu))
                     {
-                        if (is_numeric($cpu))
-                        {
-                            $info['cpu']['us']=$cpu;
-                            break;
-                        }
+                        $info['cpu']['us']=$cpu;
+                        break;
                     }
                 }
-
-                //内存不这么取了
-                //preg_match_all('/Mem/',$tmp[1],$thisRes);
-
-                //if (!empty(current($thisRes)))
-                //{
-                //    $info['mem']=['totle'=>$tmp[3],'free'=>$tmp[5],'used'=>$tmp[8]];
-                //}
-
-                preg_match_all('/Swap/',$tmp[1],$thisRes);
-
-                if (!empty(current($thisRes)))
-                {
-                    $info['swap']=['totle'=>$tmp[2],'free'=>$tmp[4],'used'=>$tmp[13]];
-                }
             }
 
-            //内存用free -h取
-            $fp=popen("free -m | egrep 'Mem'","r");
+            //内存不这么取了
+            //preg_match_all('/Mem/',$tmp[1],$thisRes);
 
-            $rs="";
+            //if (!empty(current($thisRes)))
+            //{
+            //    $info['mem']=['totle'=>$tmp[3],'free'=>$tmp[5],'used'=>$tmp[8]];
+            //}
 
-            while(!feof($fp))
+            preg_match_all('/Swap/',$tmp[1],$thisRes);
+
+            if (!empty(current($thisRes)))
             {
-                $rs .= fread($fp,1024);
+                $info['swap']=['totle'=>$tmp[2],'free'=>$tmp[4],'used'=>$tmp[13]];
             }
-
-            pclose($fp);
-
-            $res=current(array_filter(explode(PHP_EOL,$rs)));
-
-            $res=array_values(array_filter(explode(' ',$res)));
-
-            $per=$res[2] / $res[1] * 100;
-
-            $info['mem']=['totle'=>round($res[1]/1024,0),'used'=>round($res[2]/1024,0),'free'=>round($res[3]/1024,1),'per'=>round($per,1).'%'];
-
-            //查看负载
-            //load average
-            $fp=popen("top -b -n 2 | egrep 'load average' | head -1","r");
-
-            $rs=fread($fp,1024);
-
-            pclose($fp);
-
-            $res=current(array_filter(explode(PHP_EOL,$rs)));
-
-            $res=explode(' ',$res);
-
-            $arrCount=count($res);
-
-            $num=rtrim($res[$arrCount-3],',');
-
-            $info['cpu']['loadAverage']=rtrim($num,',');
-
-            //查看磁盘
-            $fp=popen('df -lh | egrep "/dev/sda3"',"r");
-
-            $rs=fread($fp,1024);
-
-            pclose($fp);
-
-            $res=array_filter(explode(PHP_EOL,$rs));
-
-            $res=current($res);
-
-            $res=preg_replace("/\s{2,}/",' ',$res);
-
-            $res=explode(' ',$res);
-
-            $info['disk']=['name'=>$res[0],'totle'=>$res[1],'used'=>$res[2],'free'=>$res[3],'percentage'=>$res[4]];
-
-            $info['lastUpdate']=time();
-
-            $info['fogUploadNum']=$this->fogUploadNum();
-
-            Redis::connection('default')->set('ServerInfo',jsonEncode($info));
-
-            sleep(17);
         }
+
+        //内存用free -h取
+        $fp=popen("free -m | egrep 'Mem'","r");
+
+        $rs="";
+
+        while(!feof($fp))
+        {
+            $rs .= fread($fp,1024);
+        }
+
+        pclose($fp);
+
+        $res=current(array_filter(explode(PHP_EOL,$rs)));
+
+        $res=array_values(array_filter(explode(' ',$res)));
+
+        $per=$res[2] / $res[1] * 100;
+
+        $info['mem']=['totle'=>round($res[1]/1024,0),'used'=>round($res[2]/1024,0),'free'=>round($res[3]/1024,1),'per'=>round($per,1).'%'];
+
+        //查看负载
+        //load average
+        $fp=popen("top -b -n 2 | egrep 'load average' | head -1","r");
+
+        $rs=fread($fp,1024);
+
+        pclose($fp);
+
+        $res=current(array_filter(explode(PHP_EOL,$rs)));
+
+        $res=explode(' ',$res);
+
+        $arrCount=count($res);
+
+        $num=rtrim($res[$arrCount-3],',');
+
+        $info['cpu']['loadAverage']=rtrim($num,',');
+
+        //查看磁盘
+        $fp=popen('df -lh | egrep "/dev/sda3"',"r");
+
+        $rs=fread($fp,1024);
+
+        pclose($fp);
+
+        $res=array_filter(explode(PHP_EOL,$rs));
+
+        $res=current($res);
+
+        $res=preg_replace("/\s{2,}/",' ',$res);
+
+        $res=explode(' ',$res);
+
+        $info['disk']=['name'=>$res[0],'totle'=>$res[1],'used'=>$res[2],'free'=>$res[3],'percentage'=>$res[4]];
+
+        $info['lastUpdate']=time();
+
+        $info['fogUploadNum']=$this->fogUploadNum();
+
+        Redis::connection('default')->set('ServerInfo',jsonEncode($info));
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -57,16 +57,16 @@ class Kernel extends ConsoleKernel
 
 
         //处理探索世界迷雾
-        $schedule->command('Tssj:FogUpload0')->everyFiveMinutes()->withoutOverlapping();
-        $schedule->command('Tssj:FogUpload1')->everyFiveMinutes()->withoutOverlapping();
-        $schedule->command('Tssj:FogUpload2')->everyFiveMinutes()->withoutOverlapping();
-        $schedule->command('Tssj:FogUpload3')->everyFiveMinutes()->withoutOverlapping();
-        $schedule->command('Tssj:FogUpload4')->everyFiveMinutes()->withoutOverlapping();
-        $schedule->command('Tssj:FogUpload5')->everyFiveMinutes()->withoutOverlapping();
-        $schedule->command('Tssj:FogUpload6')->everyFiveMinutes()->withoutOverlapping();
-        $schedule->command('Tssj:FogUpload7')->everyFiveMinutes()->withoutOverlapping();
-        $schedule->command('Tssj:FogUpload8')->everyFiveMinutes()->withoutOverlapping();
-        $schedule->command('Tssj:FogUpload9')->everyFiveMinutes()->withoutOverlapping();
+        $schedule->command('Tssj:FogUpload0')->everyMinute()->withoutOverlapping();
+        $schedule->command('Tssj:FogUpload1')->everyMinute()->withoutOverlapping();
+        $schedule->command('Tssj:FogUpload2')->everyMinute()->withoutOverlapping();
+        $schedule->command('Tssj:FogUpload3')->everyMinute()->withoutOverlapping();
+        $schedule->command('Tssj:FogUpload4')->everyMinute()->withoutOverlapping();
+        $schedule->command('Tssj:FogUpload5')->everyMinute()->withoutOverlapping();
+        $schedule->command('Tssj:FogUpload6')->everyMinute()->withoutOverlapping();
+        $schedule->command('Tssj:FogUpload7')->everyMinute()->withoutOverlapping();
+        $schedule->command('Tssj:FogUpload8')->everyMinute()->withoutOverlapping();
+        $schedule->command('Tssj:FogUpload9')->everyMinute()->withoutOverlapping();
 
 
 
